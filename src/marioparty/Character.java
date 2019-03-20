b7599e514f13240617819140abece10aa9437688
Arnav Mehrotra <arnavmehrotra.2@gmail.com>
2019-03-20 09:13:45 -0400

    Made basics about characters.

--- src/marioparty/Character.java
+++ src/marioparty/Character.java
@@ -13,7 +13,17 @@ import DLibX.DConsole;
  */
 public class Character {
 
+    public enum CharacterName {
+        LUIGI, WARIO, WALUIGI, YOSHI, GOOMBA, BOWSER, PEACH, PIRANHA_PLANT, 
+        DONKEY_KONG, MR_L
+    }
+    
     private final DConsole dc;
+    private CharacterName name;
+    private int x;
+    private int y;
+    private int coins;
+    private int stars;
 
     public Character(DConsole dc) {
         this.dc = dc;
@@ -25,5 +35,14 @@ public class Character {
 
     public void draw() {
 
+    }
+    
+    public void check() {
+        if (this.coins < 0) {
+            this.coins = 0;
+        } 
+        if (this.stars < 0) {
+            this.stars = 0;
+        }
     }
 }
