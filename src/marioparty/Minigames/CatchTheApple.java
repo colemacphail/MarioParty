1d23fa82c99d94957b0779f743e9dfcece66bb26
jacobcarson877 <48719681+jacobcarson877@users.noreply.github.com>
2019-04-16 09:11:59 -0400

    Catch The Apple v 0.0.4
    Catch the APPLE

--- src/marioparty/Minigames/CatchTheApple.java
+++ src/marioparty/Minigames/CatchTheApple.java
@@ -36,17 +36,17 @@ class Apple {
     public int getRad() {
         return this.radius;
     }
-    
-    public int getFallspeed(){
+
+    public int getFallspeed() {
         return this.fallspeed;
-        }
-    
-    public void setY(int a){
-    this.y = this.y + a;
+    }
+
+    public void setY(int a) {
+        this.y = this.y + a;
     }
 
 }
-
+ 
 class Net {
 
     private int x;
@@ -58,13 +58,13 @@ class Net {
 
     public Net() {
         this.x = 50;
-        this.y = 20;
+        this.y = 580;
         this.width = 30;
         this.height = 10;
         this.color = new Color(110, 60, 10);
-        this.movespeed = 4;
+        this.movespeed = 6;
     }
-    
+
     public int getX() {
         return this.x;
     }
@@ -72,6 +72,7 @@ class Net {
     public int getY() {
         return this.y;
     }
+
     public int getWidth() {
         return this.width;
     }
@@ -79,15 +80,24 @@ class Net {
     public int getHeight() {
         return this.height;
     }
-    public int getSpeed(){
-    return this.movespeed;
+
+    public int getSpeed() {
+        return this.movespeed;
     }
+
+    public void setX(int a) {
+        this.x += a;
+    }
+    
+    public boolean hitbox(){
+    //TODO
+    return true;
 }
 
 public class CatchTheApple extends Minigame {
 
     private Board board = Board.getInstance();
-    private Apple[] apples = new Apple[12];
+   private Apple[] apples = new Apple[12];
     private Net[] nets = new Net[board.getNumOfPlayers()];
 
     public CatchTheApple() {
@@ -100,7 +110,7 @@ public class CatchTheApple extends Minigame {
         for (int i = 0; i < this.apples.length; i++) {
             this.apples[i] = new Apple();
         }
-        for (int i = 0; i < nets.length; i ++ ){
+        for (int i = 0; i < nets.length; i++) {
             this.nets[i] = new Net();
         }
 
@@ -113,12 +123,18 @@ public class CatchTheApple extends Minigame {
             for (int i = 0; i < this.apples.length; i++) {
                 apples[i].setY(apples[i].getFallspeed());
                 this.dc.fillEllipse(this.apples[i].getX(), this.apples[i].getY(), this.apples[i].getRad(), this.apples[i].getRad());
-                
+
             }
-            for (int i = 0; i < nets.length; i ++ ){
-            this.dc.fillRect(this.nets[i].getX(), nets[i].getY(), nets[i].getWidth(), nets[i].getHeight());
+
+            for (int i = 0; i < nets.length; i++) {
+                if (this.dc.isKeyPressed(68)) {
+                    nets[i].setX(nets[i].getSpeed());
+                } else if (this.dc.isKeyPressed(65)) {
+                    nets[i].setX(-(nets[i].getSpeed()));
+                }
+                this.dc.fillRect(this.nets[i].getX(), nets[i].getY(), nets[i].getWidth(), nets[i].getHeight());
             }
-        
+
         }
     }
 
