cf852ab59bf906db63d042d7cd750b6fc1019b0f
jacobcarson877 <48719681+jacobcarson877@users.noreply.github.com>
2019-04-17 08:34:11 -0400

    oh no i forgot a comment sorry guys
    yeet

--- src/ControllerInput/GamepadInput.java
+++ src/ControllerInput/GamepadInput.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class GamepadInput {
+    //VARIABLES
     private final ControllerManager controllers;
 
     private int controllerInUse = 0;
