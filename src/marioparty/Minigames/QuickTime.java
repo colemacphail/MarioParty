e0bf019c71409ec0ca2c235589bc0916e7301e48
Arnav Mehrotra <arnavmehrotra.2@gmail.com>
2019-05-03 09:16:07 -0400

    Made minigame have graphics.

--- src/marioparty/Minigames/QuickTime.java
+++ src/marioparty/Minigames/QuickTime.java
@@ -8,9 +8,11 @@ package marioparty.Minigames;
 import ControllerInput.Controllers;
 import ControllerInput.GamepadInput;
 import ControllerInput.InputAction;
+import DLibX.DConsole;
 import java.util.Random;
 import marioparty.Board;
 import marioparty.Characters;
+import marioparty.Console;
 import marioparty.Constants;
 
 /**
@@ -30,15 +32,21 @@ class Button {
     }
 
     public boolean isPressed(int i) {
-        return controller.getControllerInput(i).actions().size() == 1 && 
-                controller.getControllerInput(i).actions().contains(buttonType);
+        return controller.getControllerInput(i).actions().size() == 1
+                && controller.getControllerInput(i).actions().contains(buttonType);
     }
+
+    public InputAction getButtonType() {
+        return buttonType;
+    }
+
 }
 
 public class QuickTime extends Minigame {
 
     private Button[] buttonList = new Button[15];
     private Characters characters;
+    private DConsole cons;
 
     public QuickTime(MinigameType type, long timeout) {
         super(type, timeout);
@@ -46,6 +54,7 @@ public class QuickTime extends Minigame {
 
     @Override
     public void init() {
+        cons = Console.getInstance();
         for (int i = 0; i < buttonList.length; i++) {
             buttonList = new Button[i];
         }
@@ -54,8 +63,13 @@ public class QuickTime extends Minigame {
     @Override
     public void run() {
         for (int i = 0; i < Constants.NUM_OF_PLAYERS; i++) {
+
+            cons.drawString(buttonList[characters.characterAtI(i).getMinigameScore()].getButtonType(),
+                    cons.getWidth() / 4 * (i % 2 == 1 ? 3 : 1),
+                    cons.getHeight() / 4 * (i > 1 ? 3 : 1));
+
             if (buttonList[characters.characterAtI(i).getMinigameScore()].isPressed(i)) {
-                characters.characterAtI(i).setMinigameScore(i);
+                characters.characterAtI(i).changeMinigameScore(1);
             }
         }
     }
